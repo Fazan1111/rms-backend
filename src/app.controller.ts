22907f253d56a526d50e37f60d493da19a39106f
Fazan Lehh <fazansolleh5@gmail.com>
2021-04-23 23:58:38 +0700

    Render data for dashboard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,14 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
+    private appService: AppService,
     private authService: AuthService
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   //@UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -23,5 +18,11 @@ export class AppController {
       password: req.body.password
     }
     return this.authService.login(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('home')
+  async index() {
+    return await this.appService.getHomeData();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { getManager } from 'typeorm';
+import { PurchaseItem } from './purchase/purchaseItem.entity';
+import SellItem from './sell/sell-item.entity';
+import { Sell } from './sell/sell.entity';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getHomeData() {
+    const purchaseItems = await getManager().createQueryBuilder(PurchaseItem, "purchaseItem")
+                      .select("SUM(purchaseItem.qty)", "qty")
+                      .getRawOne();
+
+    const saleItems = await getManager().createQueryBuilder(SellItem, "sellItem")
+                      .select("SUM(sellItem.qty)", "qty")
+                      .getRawOne();
+
+    const invoices = await getManager().createQueryBuilder(Sell, "sell")
+                     .select("COUNT(sell.invoice)", "qty")
+                     .getRawOne();
+
+    return {
+      "data" : {
+        purchaseItems,
+        saleItems,
+        invoices
+      }
+    }
+    
   }
 }
